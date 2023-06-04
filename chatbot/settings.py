4f070f7cb70110a239cb1ec8fcfa54a3ae22914c
davidhfrankelcodes <davidhfrankelcodes@gmail.com>
2023-06-03 19:08:47 -0500

    moved post-migration data to configs

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -149,3 +149,43 @@ LOGGING = {
         },
     },
 }
+
+# Post Migration Data
+## Chat User Group
+CHAT_USER_GROUP = {
+            'name': 'Chat User',
+            'permissions': [
+                'web.view_chatbot',
+                'web.view_conversation',
+                'web.add_conversation',
+                'web.change_conversation',
+                'web.delete_conversation',
+                'web.view_message',
+                'web.change_message',
+                'web.add_message',
+                'web.delete_message',
+            ]
+        }
+# Default Chatbots
+DEFAULT_CHATBOTS = [
+        {
+            'name': 'chatbot01',
+            'system_message_prompt': 'You are a helpful assistant.',
+        },
+        {
+            'name': 'stoner-bot',
+            'system_message_prompt': 'You are a helpful assistant but you are quite stoned. Your responses inevitably lead to a weed reference.',
+        },
+        {
+            'name': 'grumpy-bot',
+            'system_message_prompt': 'You are grumpy-bot. You are a grumpy assistant. You do not want to help and you are grumpy.',
+        },
+        {
+            'name': 'rhyme-bot',
+            'system_message_prompt': 'You are rhyme-bot. You are a helpful assistant and all of your responses are delightful rhymes.',
+        },
+        {
+            "name": "yoda-bot",
+            "system_message_prompt": "You are yoda-bot. You are a helpful assistant. All of your responses use yoda-speak syntax.",
+        }
+    ]
